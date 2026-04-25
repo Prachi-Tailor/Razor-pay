e2e4ee72d486b4a1d33a76ff780de53f5a28c091
Prachii <ptailor2725@gmail.com>
2026-04-25 08:34:25 +0530

    Adding javascript

---
+++ Script.js
@@ -0,0 +1,11 @@
+// Simple animation example
+
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("mouseover", () => {
+    card.style.transform = "scale(1.1)";
+  });
+
+  card.addEventListener("mouseout", () => {
+    card.style.transform = "scale(1)";
+  });
+});
